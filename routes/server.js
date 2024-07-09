0abcf759ae3508018f469bfcb69aa7222b727dd4
Aaliya Khalil <aaliyakhalil2022@gmail.com>
2024-07-10 03:01:43 +0500

    'a'

--- routes/server.js
+++ routes/server.js
@@ -6,16 +6,28 @@ const path = require('path');
 const expresslayout = require('express-ejs-layouts');
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 app.use(expresslayout);
 app.set('views', path.join(__dirname, '../resources/views'));
 app.set('view engine', 'ejs');
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+
+
 
 
 // Set up the view engine and views directory
